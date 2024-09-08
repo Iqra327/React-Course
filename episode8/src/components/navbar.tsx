eb8bcd0c13e7997efa3f4f75af0fbf12282072df
Iqra327 <iqrarasheed219@gmail.com>
2024-09-09 00:19:07 +0500

    iniitial-commit

---
+++ episode8/src/components/navbar.tsx
@@ -0,0 +1,35 @@
+import { Link } from "react-router-dom";
+import { auth } from "../config/firebase";
+import {useAuthState} from 'react-firebase-hooks/auth';
+import { signOut } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+
+export const Navbar = () => {
+  const [user] = useAuthState(auth);
+  const navigate = useNavigate();
+
+  const signUserOut = async () => {
+    await signOut(auth);
+    navigate('/login');
+  }
+  return (
+    <div>
+      <Link to='/'> Home </Link>
+      {!user ? (
+        <Link to='/login'>Login</Link> 
+      ) : ( 
+        <Link to='/createpost'>Create Post</Link> 
+      )}
+      <div>
+
+        {user && (
+          <>
+            <p>{user?.displayName}</p>
+            <img src="{user?.photoURL || ''}" width='20' height='20'/>
+            <button onClick={signUserOut}>Log Out</button>
+          </>
+        )}
+      </div>
+    </div>
+  )
+}
---
+++ episode8/src/config/firebase.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getAuth, GoogleAuthProvider} from 'firebase/auth';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAhAVSp915ODi_4KNGGQDnLYnsFbB4MqqI",
+  authDomain: "react-course-fbc94.firebaseapp.com",
+  projectId: "react-course-fbc94",
+  storageBucket: "react-course-fbc94.appspot.com",
+  messagingSenderId: "433106381300",
+  appId: "1:433106381300:web:dc602627e7bf66980c31a1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
